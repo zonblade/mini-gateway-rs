80c877e7237bdd5cee7be426499fc600c267bd65
zonblade <agungzonblade@gmail.com>
2025-04-03 14:37:44 +0900

    update some deps

--- router-gui/web-gui/svelte.config.js
+++ router-gui/web-gui/svelte.config.js
@@ -1,11 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: null,
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
