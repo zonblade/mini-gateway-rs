e859bfc6ad9c725874c1bc29fe62af374e147d45
zonblade <agungzonblade@gmail.com>
2025-04-10 15:31:16 +0900

    remove unnecesary thing

--- router-core/src/system/netlisten/mod.rs
+++ router-core/src/system/netlisten/mod.rs
@@ -5,8 +5,6 @@ pub mod packet_handler;
 
 mod listener;
 
-pub use listener::NetworkListener;
-
 // Re-export the network_capture function
 pub fn network_capture() {
     crate::system::netlisten::listener::network_capture()
